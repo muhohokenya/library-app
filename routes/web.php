16c1f2bcc89e360eec861ee50d24091fc1c42345
muhohoweb <muhohoweb@gmail.com>
2025-11-01 11:47:20 +0300

    Redirect home

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin/login');
 })->name('home');
 
 Route::view('dashboard', 'dashboard')
